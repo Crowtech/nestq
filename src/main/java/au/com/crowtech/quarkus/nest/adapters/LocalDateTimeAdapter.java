0cdb1003d5a7d5bf492cf1232431c604a685f7c1
Bryn <rhysmchaen@gmail.com>
2021-09-05 15:13:18 +1000

    LocalDateTimeAdapterDebug

--- src/main/java/au/com/crowtech/quarkus/nest/adapters/LocalDateTimeAdapter.java
+++ src/main/java/au/com/crowtech/quarkus/nest/adapters/LocalDateTimeAdapter.java
@@ -1,10 +1,12 @@
 package au.com.crowtech.quarkus.nest.adapters;
 
+import java.lang.invoke.MethodHandles;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.logging.Logger;
 
 import javax.json.Json;
 import javax.json.JsonValue;
@@ -14,13 +16,15 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class LocalDateTimeAdapter implements JsonbAdapter<LocalDateTime, JsonValue> {
-	 private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-	 
-	 public LocalDateTimeAdapter() {}
+	private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+	public LocalDateTimeAdapter() {
+	}
 
 	@Override
 	public JsonValue adaptToJson(LocalDateTime obj) throws Exception {
-		
+
 		String dateTimePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(dateTimePattern);
 		String localDateTimeStr = obj.format(dateFormatter);
@@ -30,13 +34,14 @@ public class LocalDateTimeAdapter implements JsonbAdapter<LocalDateTime, JsonVal
 
 	@Override
 	public LocalDateTime adaptFromJson(JsonValue obj) throws Exception {
+		log.info(obj.toString());
 		String dateTimePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(dateTimePattern);
 		String str = obj.toString();
-		str = str.substring(0,str.length()-1);
+		str = str.substring(0, str.length() - 1);
 		String value = str.substring(1);
 		LocalDateTime ret = null;
-		
+
 		try {
 			ret = dateFormatter.parse(value, LocalDateTime::from);
 		} catch (Exception e) {
@@ -45,14 +50,13 @@ public class LocalDateTimeAdapter implements JsonbAdapter<LocalDateTime, JsonVal
 			ret = dateFormatter.parse(value, LocalDateTime::from);
 		}
 
-	return ret;
+		return ret;
 	}
-	
-	static public  LocalDateTime getLocalDateTimeFromString(final String dateTimeStr, ZoneOffset zoneOffset)
-	{
+
+	static public LocalDateTime getLocalDateTimeFromString(final String dateTimeStr, ZoneOffset zoneOffset) {
 		TemporalAccessor ta = DateTimeFormatter.ISO_INSTANT.parse(dateTimeStr);
-	    Instant i = Instant.from(ta);
-	    LocalDateTime dt  = LocalDateTime.ofInstant(i, zoneOffset);
-	    return dt;
+		Instant i = Instant.from(ta);
+		LocalDateTime dt = LocalDateTime.ofInstant(i, zoneOffset);
+		return dt;
 	}
 }
